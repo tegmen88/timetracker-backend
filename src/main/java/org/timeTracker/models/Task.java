9382cd192cd1f0a0644137aa845226aedef0d417
tegmen88 <cite7173@gmail.com>
2024-05-01 12:01:15 +0200

    models

--- src/main/java/org/timeTracker/models/Task.java
+++ src/main/java/org/timeTracker/models/Task.java
@@ -9,4 +9,35 @@ public class Task extends PanacheMongoEntity {
     public String description;
     public Date startTime;
     public Date endTime;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDuration() {
+        return duration;
+    }
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Date getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+    public Date getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    } 
 }
